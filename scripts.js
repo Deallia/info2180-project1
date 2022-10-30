2fb5ae60947814baec687f07c244e28b28024255
Deallia Dunbar <dealliadunbar@gmail.com>
2022-10-30 18:03:36 -0500

    Edited scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-/* Add your JavaScript to this file */
+
+window.onload = (event) => {
+
+
+const button = document.querySelector("button");
+
+const btnListener =() => button.addEventListener("click", handleClick);
+
+
+function handleClick(event){
+event.preventDefault();
+var email = document.querySelector("input").value;
+       console.log("Thank you! your email " + email +
+" has been added to our mailing list!");
+    
+	document.querySelector(".message").innerHTML= "Thank you! your email " + email +
+" has been added to our mailing list!";
+    
+}
+
+
+
+	btnListener();
+};
+
